b5e212452511d6d58702617b0fd0669c4e299138
loremIpsum1771 <loremIpsum1771@users.noreply.github.com>
2015-09-05 16:20:41 -0400

    Update scrapy_script.sh

--- scrapy_script.sh
+++ scrapy_script.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
-# My first script
+#!/usr/bin/expect
 
-#echo "Hello World!"
+
 cd ~/Desktop/scrapers_repo
 python clear_db.py
 cd ~/Desktop/scrapers_repo/vividseats_scraper
 scrapy crawl comparator
+# spawn scrapy crawl comparator
+# expect "Enter a bandname \n"
+# send "alt-j"
 cd ~/Desktop/scrapers_repo/ticket_city_scraper
 scrapy crawl comparator
+# spawn scrapy crawl comparator
+# expect "Enter a bandname \n"
+# send "alt-j"
 cd ~/Desktop/scrapers_repo
 python priceGrapher.py
 
-#export PATH=$PATH:directory
